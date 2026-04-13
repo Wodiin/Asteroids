01b35702eae6f27e4ffbf7e839398373d4bc68cf
Wodiin <samphillips246@gmail.com>
2026-04-13 16:56:52 +1000

    added delta time

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ def main():
     print("Screen height: " + str(constants.SCREEN_HEIGHT))
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    screen.fill("black")
-    display.flip()
+        screen.fill("black")
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
+
 if __name__ == "__main__":
     main()
